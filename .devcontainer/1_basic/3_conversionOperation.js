a8ebb59bfecfeb8cbfcaa568347efc1052142af6
Yadavvamrita <163735642+Yadavvamrita@users.noreply.github.com>
2024-06-21 12:11:54 +0000

    conversionofDtaType

---
+++ .devcontainer/1_basic/3_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33abc"
+
+//////console.log(typeof score);
+////console.log( typeof (score));
+//if score is written as "33" then it will be treated as string
+
+let valueInNumber = Number(score)
+
+ //////console.log(typeof valueInNumber);
+ ///console.log(valueInNumber);
+//check the value of score if it is null, undefined , true or false,..... check the output
+//conversion
+//'33"= 33
+//"33abc" = nan
+//true=1 and false =0
+
+
+
+let isLoggedIn = 1    // =>//give true
+//let isLoggedIn = ""     =>output is false
+//let isLoggedIn ="amrita"    => output is true 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log( typeof stringNumber);
