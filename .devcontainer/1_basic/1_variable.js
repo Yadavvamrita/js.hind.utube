a52d78c31fbd9263d6d416c16926346ac4c3c25a
Yadavvamrita <163735642+Yadavvamrita@users.noreply.github.com>
2024-06-21 11:32:01 +0000

    dataType

---
+++ .devcontainer/1_basic/1_variable.js
@@ -0,0 +1,17 @@
+const accountId = 1632637
+let accountEmail = "amritabdsbfuh"
+var accountPassword = "1324"
+accountCiyy = "jaipur"
+let qaccountState
+// accountId =5    not allowed
+
+accountPassword= '7843785438' 
+accountEmail="uuhfewhfher"
+accountCiyy='jhewwb'
+console.log(accountId);
+/*
+prefer not to use var, because of issue of block scope 
+and function scope
+
+*/
+console.table([accountEmail,accountId , accountPassword, accountCiyy , accountState])
---
+++ .devcontainer/1_basic/2_dataType.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello")// we are using node js not browser 
+let name = "amrita"
+let age =18
+let isLoggedIn = false
+
+
+
+// number => 2 to the power of 53(range)
+//bigint 
+// string => use in double cote
+//boolean => true/ false
+// null => standalone value of empty value 
+// undefined
+//null os a object
+
+console.log(typeof"amrita")
+console.log(typeof "age")
+console.log(typeof undefined)//output is undefined
+console.log(typeof null) // output as a object
