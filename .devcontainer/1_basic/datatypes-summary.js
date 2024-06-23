f95cbbb5a05a929f13bbaf90aa2afb593ba0ea48
Yadavvamrita <163735642+Yadavvamrita@users.noreply.github.com>
2024-06-23 04:04:54 +0000

    datatype summary

---
+++ .devcontainer/1_basic/datatypes-summary.js
@@ -0,0 +1,39 @@
+//datatypes are ...
+//1 primitive
+// primitive are 1 string, 2number, 3boolean, 4 null,
+//5undefined, 6 symbol, 7BigInt
+
+//reference type(non primitive
+// array, object, function are not primitive)
+
+
+//check whether  java script is ststic or dynamic
+
+
+const score =100
+const scoreValue = 223.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = symbol('123')
+const anotherId = symbol('123')
+console.log(id===anotherId);
+
+const bigNumber= 38472675n
+
+
+
+const heros= ["shaktiman", "naagraj", "doga"]
+ let myObj= {
+    name: "amrita",
+    age : 12,
+
+}
+
+
+const myFunction = function(){
+    console.log("Hello Word");
+}
+console.log(typeof bigNumber);/// type of outside temp is object
+
+
