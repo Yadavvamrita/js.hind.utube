1e726ce723aafa19a7b80a710954c9577c28c0ae
Yadavvamrita <163735642+Yadavvamrita@users.noreply.github.com>
2024-06-24 18:26:23 +0000

    Maths and num

---
+++ .devcontainer/1_basic/6_nums_and_maths.js
@@ -0,0 +1,33 @@
+const score = 400
+//console.log(score)
+
+
+const balance = new Number (100)
+  //  console.log(balance)
+
+   // console.log(balance.toString().length); 
+   //// console.log(balance.toFixed(2)); // to give pricision value
+    const otherNumber = 23.8745
+   // console.log(otherNumber.toPrecision(3));// CHECK FOR TO PRECISION (3) FOR NUMBER 123.8745
+
+
+    const hundreds = 1000000
+   // console.log(hundreds.toLocaleString('en-IN'));      // CHECK THE NUMBER OF ZERO
+
+
+    //++++++++++           MATHS     ++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));       
+//console.log(Math.round(4.32));
+//console.log(Math.ceil(4.6));
+//console.log(Math.floor(484.74));
+//console.log(Math.min(3,5,65,22)); // give min value 
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 10
+const max =20
+
+console.log(Math.floor(Math.random()*(max-min +1)) + min) // to get the number btw 10 and 20
+ 
