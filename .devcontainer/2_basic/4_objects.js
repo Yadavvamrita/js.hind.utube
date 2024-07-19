8452851f8c1cdfce9bcab25f73f662bda8db86de
Yadavvamrita <163735642+Yadavvamrita@users.noreply.github.com>
2024-07-19 17:43:28 +0000

    json

---
+++ .devcontainer/2_basic/4_objects.js
@@ -0,0 +1,95 @@
+//  const tinderUser = new Object()       this is singleton object
+//    const tinderUser = {}   this is not singlrton
+const tinderUser ={}
+
+// tinderUser.id = "231263gh"
+// tinderUser.name = "amru"
+// tinderUser.isLoggedIn = false
+//console.log(tinderUser);
+
+// const regularUser ={
+//     email: 'some@gmail.com',
+//     fullname:{
+//         userfullname:{
+//             firstname :"amru",
+//             lastname:"yadav"
+
+//         }
+
+//     }
+// }
+// //console.log(regularUser.fullname.userfullname.firstname);
+// const obj1 = {1:"a", 2:"b"}
+// const obj2 = {3:"a", 4:"b"}
+// const obj4 = {5:"a", 6:"b"}
+// //const obj3 = {obj1, obj2}
+// //const obj3 = Object.assign({}, obj1, obj2, obj4)/// very important point see it
+// const obj3 = {...obj1, ...obj2}
+// //console.log(obj3);
+
+
+// ARRAY AS OBJECT
+// const users = [
+//     {
+//         id:1,
+//         email:"amru@gmail.com"
+//     },
+//     {
+//         id:1,
+//         email:"amru@gmail.com"
+//     },
+//     {
+//         id:1,
+//         email:"amru@gmail.com"
+//     },
+//     {
+//         id:1,
+//         email:"amru@gmail.com"
+//     },
+//     {
+//         id:1,
+//         email:"amru@gmail.com"
+//     },
+// ]
+
+
+// users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+
+// console.log(Object.values(tinderUser)); 
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));  // boolean vaue
+
+
+// const course = {
+//     coursename:"js in hindi",
+//     price: "99",
+//     courseInstructor:"amru"
+// }
+// ///course.courseInstructor   //not a good practice
+// const  {courseInstructor} = course  // below syntax is a convinient code we o
+// //have to write instructor to call the object
+
+// const  {courseInstructor: instructor} = course
+// //console.log(courseInstructor);
+// console.log(instructor);  // can say better than courseinstructor
+
+// {
+//     "name":"amrita",
+//     "coursenames": "js in hindi",     // jason format it is
+//     "price" : "free"
+
+// }
+[
+    {
+         
+    }
+]
+
+
+////////see this video from 8 min  for api
+
+
